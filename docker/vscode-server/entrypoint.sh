92f2048b256917ae22aabeb285bcb105456e6ab7
Colin Tickle <colin.tickle@gmail.com>
2023-04-18 16:09:36 +0100

    vscode-server: including extensions, PHP support and enabling secure access

--- docker/vscode-server/entrypoint.sh
+++ docker/vscode-server/entrypoint.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -z "$RUNNING_PROJECT" ]; then
-  echo -e "RUNNING_PROJECT is not defined\nSTART WITH:\nRUNNING_PROJECT=RUNNING_PROJECT docker-compose up phpstorm-server";
+  echo -e "RUNNING_PROJECT is not defined\nSTART WITH:\nRUNNING_PROJECT=RUNNING_PROJECT docker-compose up vscode-server";
   exit 1;
 fi
 
-EXTENSIONS=redhat.vscode-yaml,vscode-icons-team.vscode-icons
-
-mkdir -p /var/www/htdocs/.config/code-server/
-cat > /var/www/htdocs/.config/code-server/config.yaml << EOF
-bind-addr: 0.0.0.0:8433
-auth: none
-cert: false
-EOF
+EXTENSIONS=redhat.vscode-yaml,vscode-icons-team.vscode-icons,esbenp.prettier-vscode,bmewburn.vscode-intelephense-client,mikestead.dotenv,ikappas.phpcs,neilbrayfield.php-docblocker
 
 echo "Installing Extensions"
 for extension in $(echo ${EXTENSIONS} | tr "," "\n")
@@ -25,9 +18,21 @@ for extension in $(echo ${EXTENSIONS} | tr "," "\n")
     fi
   done
 
-/usr/bin/code-server \
-  --auth none \
-  --bind-addr "0.0.0.0":"8443" \
-#  --cert /var/www/certs/0.0.0.0.crt \
-#  --cert-key /var/www/certs/0.0.0.0.key \
-  "/var/www/htdocs/${RUNNING_PROJECT}/"
+mkdir -p /var/www/htdocs/.config/code-server/
+cat > /var/www/htdocs/.config/code-server/config.yaml << EOF
+bind-addr: 0.0.0.0:8433
+auth: none
+cert: false
+EOF
+
+if [[  -n "$VSCODE_SECURE" && "$VSCODE_SECURE" -eq 1 ]]; then
+  /usr/bin/code-server \
+    --bind-addr "0.0.0.0":"8443" \
+    --cert /var/www/certs/0.0.0.0.crt \
+    --cert-key /var/www/certs/0.0.0.0.key \
+    "/var/www/htdocs/${RUNNING_PROJECT}/"
+else
+    /usr/bin/code-server \
+      --bind-addr "0.0.0.0":"8443" \
+      "/var/www/htdocs/${RUNNING_PROJECT}/"
+fi
